f03d17c92f47e67ea171109f80f66c9c87982279
Stiletto <blasux@blasux.ru>
2017-02-13 00:02:34 +0300

    gofmt

--- main.go
+++ main.go
@@ -1,153 +1,156 @@
 package main
+
 import (
-    "bytes"
-    "encoding/json"
-    "flag"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net"
-    "net/http"
-    "net/url"
-    "os"
-    "strings"
-    "sync/atomic"
-    "time"
-    "github.com/golang/groupcache"
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/golang/groupcache"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"sync/atomic"
+	"time"
 )
 
 var (
-    configName string
+	configName string
 )
 
 type StatusStruct struct {
-    RequestsProcessed uint64
-    ThumbsGenerated uint64
-    ThumbsFailed uint64
+	RequestsProcessed uint64
+	ThumbsGenerated   uint64
+	ThumbsFailed      uint64
 }
 
 var status StatusStruct
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method != "GET" {
-        http.Error(w,"Method not allowed", http.StatusMethodNotAllowed)
-        return
-    }
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
-    hdr := w.Header()
+	hdr := w.Header()
 
-    hdr.Set("Content-Type", "application/json")
-    stjson, _ := json.MarshalIndent(status,"", "  ")
-    w.Write(stjson)
+	hdr.Set("Content-Type", "application/json")
+	stjson, _ := json.MarshalIndent(status, "", "  ")
+	w.Write(stjson)
 }
 
 func thumbHandler(w http.ResponseWriter, r *http.Request) {
-    atomic.AddUint64(&status.RequestsProcessed, 1)
-    if r.Method != "GET" {
-        http.Error(w,"Method not allowed", http.StatusMethodNotAllowed)
-        return
-    }
-    uri := strings.SplitN(r.RequestURI, "/",3)
-    if len(uri) != 3 || uri[0] != "" {
-        http.Error(w,"Malformed URI", 500)
-        return
-    }
+	atomic.AddUint64(&status.RequestsProcessed, 1)
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	uri := strings.SplitN(r.RequestURI, "/", 3)
+	if len(uri) != 3 || uri[0] != "" {
+		http.Error(w, "Malformed URI", 500)
+		return
+	}
 
-    var ctx groupcache.Context
-    var data []byte
-    err := Group.Get(ctx, uri[2], groupcache.AllocatingByteSliceSink(&data))
-    if err != nil {
-        log.Printf("GC: %s", err.Error())
-        if v, ok := err.(*HttpLoaderError); ok {
-            if v2, ok := v.What.(*url.Error); ok {
-                if v3, ok := v2.Err.(*net.OpError); ok {
-                    log.Printf("GCue: %#v", v3)
-                }
-            }
-        }
-        http.Error(w,"Unable to retreive", 404)
-        return
-    }
-    var modTime time.Time
-    http.ServeContent(w, r, "big-file-thumb.jpg", modTime, bytes.NewReader(data))
+	var ctx groupcache.Context
+	var data []byte
+	err := Group.Get(ctx, uri[2], groupcache.AllocatingByteSliceSink(&data))
+	if err != nil {
+		log.Printf("GC: %s", err.Error())
+		if v, ok := err.(*HttpLoaderError); ok {
+			if v2, ok := v.What.(*url.Error); ok {
+				if v3, ok := v2.Err.(*net.OpError); ok {
+					log.Printf("GCue: %#v", v3)
+				}
+			}
+		}
+		http.Error(w, "Unable to retreive", 404)
+		return
+	}
+	var modTime time.Time
+	http.ServeContent(w, r, "big-file-thumb.jpg", modTime, bytes.NewReader(data))
 }
 
 func init() {
-    flag.StringVar(&configName, "config", "config.json", "configuration file name")
+	flag.StringVar(&configName, "config", "config.json", "configuration file name")
 }
 
 type Configuration struct {
-    Size int64 `json:"size"`
-    Listen string `json:"listen"`
-    ListenGC string `json:"listen_gc"`
-    Me string `json:"me"`
-    Peers []string `json:"peers"`
-    Loader string `json:"loader"`
-    LoaderArgs map[string]string `json:"loader_args"`
-    MaxWidth int `json:"max_width"`
-    MaxHeight int `json:"max_height"`
-    MaxInDim int `json:"max_in_dim"`
-    JpegQuality int `json:"jpeg_quality"`
+	Size        int64             `json:"size"`
+	Listen      string            `json:"listen"`
+	ListenGC    string            `json:"listen_gc"`
+	Me          string            `json:"me"`
+	Peers       []string          `json:"peers"`
+	Loader      string            `json:"loader"`
+	LoaderArgs  map[string]string `json:"loader_args"`
+	MaxWidth    int               `json:"max_width"`
+	MaxHeight   int               `json:"max_height"`
+	MaxInDim    int               `json:"max_in_dim"`
+	JpegQuality int               `json:"jpeg_quality"`
 }
 
 var (
-    Config Configuration
-    Group *groupcache.Group
+	Config Configuration
+	Group  *groupcache.Group
 )
 
 type Handler struct {
 }
 
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path == "/status" {
-        statusHandler(w, r)
-        return
-    }
-    thumbHandler(w, r)
+	if r.URL.Path == "/status" {
+		statusHandler(w, r)
+		return
+	}
+	thumbHandler(w, r)
 }
 
 func main() {
 
-    flag.Parse()
-    file, err := os.Open(configName)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fileContents, err := ioutil.ReadAll(file)
-    if err != nil {
-        log.Fatal(err)
-    }
-    err = json.Unmarshal(fileContents, &Config)
-    if err != nil {
-        log.Fatal(err)
-    }
+	flag.Parse()
+	file, err := os.Open(configName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(fileContents, &Config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    loader, ok := loaders[Config.Loader]
-    if !ok { log.Fatalf("Loader %q doesn't exist.", Config.Loader) }
+	loader, ok := loaders[Config.Loader]
+	if !ok {
+		log.Fatalf("Loader %q doesn't exist.", Config.Loader)
+	}
 
-    renderer := &Renderer{
-        Loader: loader(Config.LoaderArgs),
-        MaxWidth: Config.MaxWidth,
-        MaxHeight: Config.MaxHeight,
-        MaxInDim: Config.MaxInDim,
-        JpegQuality: Config.JpegQuality,
-    }
+	renderer := &Renderer{
+		Loader:      loader(Config.LoaderArgs),
+		MaxWidth:    Config.MaxWidth,
+		MaxHeight:   Config.MaxHeight,
+		MaxInDim:    Config.MaxInDim,
+		JpegQuality: Config.JpegQuality,
+	}
 
-    if Config.ListenGC == "" {
-        if strings.HasPrefix(Config.Me,"http://") {
-            Config.ListenGC = Config.Me[7:len(Config.Me)]
-        }
-    }
-    peers := groupcache.NewHTTPPool(Config.Me)
-    if len(Config.Peers) > 0 {
-        peers.Set(Config.Peers...)
-        go http.ListenAndServe(Config.ListenGC, http.HandlerFunc(peers.ServeHTTP))
-    }
-    Group = groupcache.NewGroup("bnw-thumb", Config.Size<<20, renderer)
+	if Config.ListenGC == "" {
+		if strings.HasPrefix(Config.Me, "http://") {
+			Config.ListenGC = Config.Me[7:len(Config.Me)]
+		}
+	}
+	peers := groupcache.NewHTTPPool(Config.Me)
+	if len(Config.Peers) > 0 {
+		peers.Set(Config.Peers...)
+		go http.ListenAndServe(Config.ListenGC, http.HandlerFunc(peers.ServeHTTP))
+	}
+	Group = groupcache.NewGroup("bnw-thumb", Config.Size<<20, renderer)
 
-    //http.HandleFunc("/status", statusHandler)
-    //http.HandleFunc("/", thumbHandler)
-    fmt.Fprintf(os.Stderr, "Going to listen on %s\n", Config.Listen)
-    server := &http.Server{Addr: Config.Listen, Handler: Handler{}}
-    server.ListenAndServe()
+	//http.HandleFunc("/status", statusHandler)
+	//http.HandleFunc("/", thumbHandler)
+	fmt.Fprintf(os.Stderr, "Going to listen on %s\n", Config.Listen)
+	server := &http.Server{Addr: Config.Listen, Handler: Handler{}}
+	server.ListenAndServe()
 }
--- renderer.go
+++ renderer.go
@@ -1,40 +1,41 @@
 package main
+
 import (
-    "bytes"
-    "errors"
-    "image"
-    "image/png"
-    "image/jpeg"
-    "image/gif"
-    _ "golang.org/x/image/webp"
-    "io"
-    "log"
-    "strconv"
-    "strings"
-    "sync/atomic"
-    "github.com/nfnt/resize"
-    "github.com/golang/groupcache"
+	"bytes"
+	"errors"
+	"github.com/golang/groupcache"
+	"github.com/nfnt/resize"
+	_ "golang.org/x/image/webp"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"io"
+	"log"
+	"strconv"
+	"strings"
+	"sync/atomic"
 )
 
 type Renderer struct {
-    Loader SourceLoader
+	Loader SourceLoader
 
-    MaxWidth int
-    MaxHeight int
-    MaxInDim int
+	MaxWidth  int
+	MaxHeight int
+	MaxInDim  int
 
-    JpegQuality int
+	JpegQuality int
 }
 
 func (r *Renderer) Get(ctx groupcache.Context, key string, dest groupcache.Sink) error {
-    data, err := r.Render(key)
-    if err == nil {
-        atomic.AddUint64(&status.ThumbsGenerated, 1)
-        dest.SetBytes(data)
-    } else {
-        atomic.AddUint64(&status.ThumbsFailed, 1)
-    }
-    return err
+	data, err := r.Render(key)
+	if err == nil {
+		atomic.AddUint64(&status.ThumbsGenerated, 1)
+		dest.SetBytes(data)
+	} else {
+		atomic.AddUint64(&status.ThumbsFailed, 1)
+	}
+	return err
 }
 
 var RenderWrongUrl = errors.New("Wrong url format.")
@@ -43,63 +44,70 @@ var RenderWrongSize = errors.New("Wrong size.")
 var RenderTooLarge = errors.New("One of image dimensions is too large.")
 
 func (r *Renderer) Render(url string) ([]byte, error) {
-    splitUrl := strings.SplitN(url, "/", 3)
-    if len(splitUrl) != 3 {
-        return nil, RenderWrongUrl
-    }
-    if splitUrl[0] != "fit-in" {
-        return nil, RenderWrongPrefix
-    }
-    splitSize := strings.SplitN(splitUrl[1], "x", 2)
-    if len(splitSize) != 2 {
-        return nil, RenderWrongSize
-    }
+	splitUrl := strings.SplitN(url, "/", 3)
+	if len(splitUrl) != 3 {
+		return nil, RenderWrongUrl
+	}
+	if splitUrl[0] != "fit-in" {
+		return nil, RenderWrongPrefix
+	}
+	splitSize := strings.SplitN(splitUrl[1], "x", 2)
+	if len(splitSize) != 2 {
+		return nil, RenderWrongSize
+	}
 
-    width := parseOrDefault(splitSize[0], 1, r.MaxWidth, r.MaxWidth)
-    height := parseOrDefault(splitSize[1], 1, r.MaxHeight, r.MaxHeight)
+	width := parseOrDefault(splitSize[0], 1, r.MaxWidth, r.MaxWidth)
+	height := parseOrDefault(splitSize[1], 1, r.MaxHeight, r.MaxHeight)
 
-    data, err := r.Loader.Load(splitUrl[2])
-    if err != nil { return nil, err }
+	data, err := r.Loader.Load(splitUrl[2])
+	if err != nil {
+		return nil, err
+	}
 
-    bufreader := bytes.NewReader(data)
-    config, format, err := image.DecodeConfig(bufreader)
-    if err != nil { return nil, err }
-    if config.Width > r.MaxInDim || config.Height > r.MaxInDim {
-        return nil, RenderTooLarge
-    }
+	bufreader := bytes.NewReader(data)
+	config, format, err := image.DecodeConfig(bufreader)
+	if err != nil {
+		return nil, err
+	}
+	if config.Width > r.MaxInDim || config.Height > r.MaxInDim {
+		return nil, RenderTooLarge
+	}
 
-    bufreader.Seek(0,0)
-    img, _, err := image.Decode(bufreader)
-    if err != nil { return nil, err }
+	bufreader.Seek(0, 0)
+	img, _, err := image.Decode(bufreader)
+	if err != nil {
+		return nil, err
+	}
 
-    thumb := resize.Thumbnail(uint(width), uint(height), img, resize.NearestNeighbor)
-    var outbuf bytes.Buffer
-    encodeImage(&outbuf, thumb, format, r.JpegQuality)
-    log.Printf("Rendered (%dx%d %s) -> (%dx%d): %s", config.Width, config.Height, format, width, height, splitUrl[2])
-    return outbuf.Bytes(), nil
+	thumb := resize.Thumbnail(uint(width), uint(height), img, resize.NearestNeighbor)
+	var outbuf bytes.Buffer
+	encodeImage(&outbuf, thumb, format, r.JpegQuality)
+	log.Printf("Rendered (%dx%d %s) -> (%dx%d): %s", config.Width, config.Height, format, width, height, splitUrl[2])
+	return outbuf.Bytes(), nil
 }
 
 func parseOrDefault(s string, min int, max int, def int) int {
-    if s == "" {
-        return def
-    }
-    n, err := strconv.ParseInt(s, 10, 64)
-    switch {
-        case err != nil:
-            return def
-        case int(n) < min:
-            return min
-        case int(n) > max:
-            return max
-    }
-    return int(n)
+	if s == "" {
+		return def
+	}
+	n, err := strconv.ParseInt(s, 10, 64)
+	switch {
+	case err != nil:
+		return def
+	case int(n) < min:
+		return min
+	case int(n) > max:
+		return max
+	}
+	return int(n)
 }
 
 func encodeImage(w io.Writer, img image.Image, fmt string, jq int) error {
-    switch fmt {
-        case "png": return png.Encode(w, img)
-        case "gif": return gif.Encode(w, img, &gif.Options{NumColors: 256})
-    }
-    return jpeg.Encode(w, img, &jpeg.Options{Quality: jq} )
+	switch fmt {
+	case "png":
+		return png.Encode(w, img)
+	case "gif":
+		return gif.Encode(w, img, &gif.Options{NumColors: 256})
+	}
+	return jpeg.Encode(w, img, &jpeg.Options{Quality: jq})
 }
-
